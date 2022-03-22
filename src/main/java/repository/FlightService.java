b39029658c339a909737b6be890623bb2f467f38
Steve <steveboy199@gmail.com>
2022-03-22 18:42:44 -0400

    new commit

--- src/main/java/repository/FlightService.java
+++ src/main/java/repository/FlightService.java
@@ -1,12 +1,44 @@
 package repository;
 
+import DTO.models.Flight;
+import airlineHibernate.AirlineHibernateDatabase;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class FlightService {
 
-   public Boolean addFlightToDatabase(){
-      return null;
+   public Boolean addFlightToDatabase(Flight flight){
+      Boolean result = null;
+      Session session = null;
+      Transaction transaction = null;
+      AirlineHibernateDatabase airlineHibernateDatabase = AirlineHibernateDatabase.getInstance();
+      DAO.models.Flight inFlight = new DAO.models.Flight();
+      inFlight.setFlight_number(UUID.randomUUID().toString());
+      inFlight.setDepart_time(flight.getDepart_time());
+      inFlight.setLand_time(flight.getLand_time());
+      inFlight.setNumber_of_seats(flight.getNumber_of_seats());
+      inFlight.setOrigin(flight.getOrigin());
+      inFlight.setDestination(flight.getDestination());
+      try {
+
+         session = airlineHibernateDatabase.getSession();
+         transaction = session.beginTransaction();
+         transaction.begin();
+         session.save(inFlight);
+         transaction.commit();
+      }catch(Exception e){
+         result = false;
+
+      }finally{
+         if(session != null){
+            session.close();
+         }
+      }
+      return result;
    }
    public Boolean deleteFlightFromDatabase(){
       return null;
