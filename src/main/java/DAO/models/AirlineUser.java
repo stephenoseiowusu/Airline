1ffa60ccfc5ee68e80bada63a17c2e8744458a21
Steve <steveboy199@gmail.com>
2022-03-22 18:26:50 -0400

    new commit

--- src/main/java/DAO/models/AirlineUser.java
+++ src/main/java/DAO/models/AirlineUser.java
@@ -20,29 +20,5 @@ public class AirlineUser {
         this.password = password;
         this.login_id = login_id;
     }
-   /* public void setFirst_name(String first_name){
-        this.first_name = first_name;
-    }
-    public  void setLast_name(String last_name){
-        this.last_name = last_name;
-    }
-    public void setPassword(String password){
-        this.password = password;
-    }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getLogin_id() {
-        return login_id;
-    }
-
-    public void setLogin_id(String login_id) {
-        this.login_id = login_id;
-    }*/
 }
